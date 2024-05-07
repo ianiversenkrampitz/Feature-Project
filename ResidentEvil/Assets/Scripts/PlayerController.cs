e23e281005bfbac7de44bcfd07a7740716411fc7
Ian Iversen-Krampitz <72539343+ianiversenkrampitz@users.noreply.github.com>
2024-05-06 19:51:23 -0700

    Almost everything done

--- ResidentEvil/Assets/Scripts/PlayerController.cs
+++ ResidentEvil/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 /*
  * Iversen-Krampitz, Ian 
- * 04/19/2024
- * Controls player movement. 
+ * 05/04/2024
+ * Controls player movement and camera changing. 
  */
 
 public class PlayerController : MonoBehaviour
@@ -20,6 +20,7 @@ public class PlayerController : MonoBehaviour
     public bool canQuickTurn;
     public bool isQuickTurning;
     public GameObject Camera;
+    public GameObject tempCamera;
 
     // Start is called before the first frame update
     void Start()
@@ -60,12 +61,22 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("CameraChange"))
         {
-            Camera.transform.position = other.transform.parent.position;
-            Camera.transform.rotation = other.transform.parent.rotation;
-            print("changed camera.");
+            //checks if this camera is already set, changes and does cooldown if not
+            if (Camera.transform.position != other.transform.parent.position)
+            {
+                tempCamera = other.gameObject;
+                StartCoroutine(CameraCooldown());
+                print("changed camera.");
+            }
+            else
+            {
+                print("already using this camera.");
+            }
         }
     }
-
+    /// <summary>
+    /// walk movement 
+    /// </summary>
     private void Walk()
     {
         if (controls.Movement.TurnLeft.IsPressed())
@@ -93,7 +104,9 @@ public class PlayerController : MonoBehaviour
             canQuickTurn = true;
         }
     }
-
+    /// <summary>
+    /// run movement 
+    /// </summary>
     private void Run()
     {
 
@@ -123,11 +136,19 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// quickturn movement 
+    /// </summary>
     private void QuickTurn()
     {
-            transform.Rotate(Vector3.up * turnSpeed * 4 * Time.deltaTime);
+        //355 is the closest number i could find to make it a 180 degree turn 
+            transform.Rotate(Vector3.up * 355 * Time.deltaTime);
     }
 
+    /// <summary>
+    /// cooldown for quickturning 
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator QuickTurnCooldown()
     {
         moveType = QuickTurn;
@@ -137,6 +158,17 @@ public class PlayerController : MonoBehaviour
         yield return new WaitForSeconds(.5f);
         isQuickTurning = false;
         canQuickTurn = true;
+        canMove = true;
+    }
+    /// <summary>
+    /// creates a delay between camera movement to emulate load times 
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator CameraCooldown()
+    {
+        canMove = false;
+        yield return new WaitForSeconds(.2f);
+        Camera.transform.SetPositionAndRotation(tempCamera.transform.parent.position, tempCamera.transform.parent.rotation);
         canMove = true;
     }
 }
